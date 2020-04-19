ad0301452e2a809e4c517d18af24ae8963b214d5
Alessandro Lisi <61741415+PrivateAbstractAleLisi@users.noreply.github.com>
2020-04-19 15:41:15 +0200

    Pulizia Codice, NO TEST
    -rimosso commenti liberire non usate
    -aggiunti commenti utili per segnali
    -rimosso lo stato COMPLETE precedentemente commentato
    -rimosse variable dentro il process mai utilizzate
    -process sensibile reset

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -3,27 +3,17 @@
 -- Designer: Alessandro Lisi
 -- 
 -- Create Date: 26.02.2020 12:22:12
--- Last Edit: 4.04.2020
+-- Last Edit: 19.04.2020
 -- Design Name: Progetto di Reti Logiche 2020 Workzone Encoder
 -- Module Name: proj_RL_01 - Behavioral
 -- Project Name: Workzone Encoder
 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity project_reti_logiche is
 
@@ -42,44 +32,39 @@ end project_reti_logiche;
 architecture Behavioral of project_reti_logiche is
 
     type State is (S_IDLE, S_FETCH, S_FETCH2, S_RAM_WAIT, S_COMPARE, S_OFFSET, S_COMPARE2, S_SEND_ENC, S_SEND, 
-                  -- S_COMPLETE--, 
                    S_DONE, S_DONE2);
                    
-    signal current_state : State;
+    signal current_state : State; --lo stato corrente nel quale si trova la macchina 
     
-    signal ram_wait_ret_state : State;
+    signal ram_wait_ret_state : State; --utilizzato quando si passa in RAM_WAIT per sapere in che stato tornare nel ciclo 
+                                         --di clock successivo
     
 
-    signal address : std_logic_vector (7 downto 0);
-    signal counter_3 : std_logic_vector (3 downto 0) := (others => '0');
+    signal address : std_logic_vector (7 downto 0); --indirizzo letto
+    signal counter_3 : std_logic_vector (3 downto 0) := (others => '0'); --contatore usato per iterare le working zones
     
-    signal hp0, hp1, hp2, hp3 : std_logic_vector (7 downto 0) := (others => '0');
-    signal cmp_wz_found : std_logic;
+    signal hp0, hp1, hp2, hp3 : std_logic_vector (7 downto 0) := (others => '0'); --calcolo degli indirizzi interni alla wz
+    signal cmp_wz_found : std_logic; --bit che viene messo a 1 quando ho un match (address coincide con un indirizzo di una wz)
     signal cmp_wz_num : std_logic_vector (2 downto 0);
     signal cmp_wz_offset_one_hot : std_logic_vector(3 downto 0);
 
-    
-
-
-    
+ 
 begin
 
 
-algo: process (i_clk) is
+algo: process (i_clk, i_rst) is --singolo processo sensibile al clock e al reset
 
-       variable WZ_NUM : integer := -1;
-       variable WZ_OST : integer := -1;
 
       begin
       
        if i_rst = '1' then
+       
             current_state <= S_IDLE; --mi metto nello stato di IDLE
             o_done <= '0'; -- resetto i segnali di uscita
-            o_en <= '0';
+            o_en <= '0'; --non voglio ne leggere ne scrivere
             o_we <= '0';
             counter_3 <= "0000";  --inizializzo contatore WZ
-            cmp_wz_found <= '0';  
-            --TODO altre inizializzazioni
+            cmp_wz_found <= '0';   --inizializzo bit wz trovata
 
 
        elsif i_clk'event and rising_edge(i_clk) then
@@ -92,7 +77,7 @@ algo: process (i_clk) is
                if i_start = '1' then
                   current_state <= S_FETCH;
                end if;
-          --aspetta 1ck per ottenere il dato letto dalla ram  
+          --aspetta 1ck per ottenere il dato letto dalla ram, successivamente torna nello stato indicato in ram wait return state
             when S_RAM_WAIT => 
                 current_state <= ram_wait_ret_state;
           --legge l'indirizzo della ram
@@ -101,16 +86,17 @@ algo: process (i_clk) is
                o_en <= '1';
                counter_3 <= "0000";  --inizializzo contatore WZ e i vari segnali ausiliari
                cmp_wz_found <= '0';
-
-               --leggo l'indirizzo da codificare
+               
+               --leggo l'indirizzo da codificare, si trova nella 8va posizione
                o_address <= std_logic_vector(to_unsigned( 8 , 16)); 
 
                ram_wait_ret_state <= S_FETCH2;
                current_state <= S_RAM_WAIT;
+               
             when S_FETCH2 =>
 
-               address <= i_data;
-               ram_wait_ret_state <= S_COMPARE;
+               address <= i_data;  --salvo l'indirizzo letto nel segnale address
+               ram_wait_ret_state <= S_COMPARE; 
                current_state <= S_RAM_WAIT;
 
             when S_COMPARE =>
@@ -119,7 +105,7 @@ algo: process (i_clk) is
                      ram_wait_ret_state <= S_OFFSET;
                      current_state <= S_RAM_WAIT;
                      counter_3 <= counter_3 + 1;           
-  --se non appartiene a nessuna WZ il contatore arriva ad 8:                           
+            --se non appartiene a nessuna WZ il contatore arriva ad 8:                           
                elsif counter_3 = "1000" then  
                     counter_3 <= "0000";
                     current_state <= S_SEND;
@@ -135,7 +121,7 @@ algo: process (i_clk) is
              --chiedo di leggere l'indirizzo di counter
              -------------------------------------------------------------------
              o_address <= std_logic_vector(resize(unsigned(counter_3), 16)); 
-             --impongo che found e i vari sotto campi siano a 0.
+             --inizializzo found e i vari sotto campi  a 0.
              cmp_wz_found <= '0';
              cmp_wz_num <= "000";
              cmp_wz_offset_one_hot <= "0000";
@@ -151,9 +137,9 @@ algo: process (i_clk) is
                   current_state <= S_COMPARE2;
                   --if hp0 xnor 
                
-            when S_COMPARE2 =>
+            when S_COMPARE2 => --data una wz controlla se c'è un match tra address e i 4 possibili indirizzi hp0,1,2,3
                                   
-            --esegui i controlli: guarda se l'indirizzo matcha con WZ + 0,1,2,3
+            --esegui i controlli: guarda se l'indirizzo matcha con WZ + 0,1,2,3, se c'è un match riempi i campi dell'indirizzo codificato
             if (hp0 = address) then
               cmp_wz_found <= '1';
               cmp_wz_num <= counter_3(2 downto 0) -1 ;
@@ -176,13 +162,13 @@ algo: process (i_clk) is
                 current_state <= S_SEND_ENC;             
             else 
                 current_state <= S_COMPARE;
-                cmp_wz_found <= '0'; --non ho trovato un'appartenenza ad una wz
+                cmp_wz_found <= '0'; --non ho trovato un'appartenenza ad una wz, ritorno a compare 
             end if;
             
             
 
             --INVIO: 
-            when s_send =>
+            when s_send =>  --quando non ho nessun match e lo invio cosi come è
                 o_en <= '1'; -- voglio accedere alla ram e scriverci
                 o_we <= '1';
                 o_data <= address;  --indirizzo dry non modificato
@@ -190,32 +176,26 @@ algo: process (i_clk) is
                 ram_wait_ret_state <= S_DONE;
                 current_state <= S_RAM_WAIT;
                 
-            when s_send_enc =>
+            when s_send_enc => --ho trovato un match in una delle wz, combino i campi compilati in precedenza per ottenere l'indirizzo codificato
                 
-                 o_data <= '1' & cmp_wz_num & cmp_wz_offset_one_hot;
+                 o_data <= '1' & cmp_wz_num & cmp_wz_offset_one_hot; --l'indirizzo codificato come concatenazione dei sotto campi
                  o_address <= std_logic_vector(to_unsigned( 9 , 16));
                  o_we <= '1';
                  ram_wait_ret_state <= S_DONE;
                  current_state <= S_RAM_WAIT;
-            --lo tolgo?     
---            when S_COMPLETE =>
---                o_en <= '1';
---                o_we <= '1';
---             ram_wait_ret_state <= S_DONE;
---             current_state <= S_RAM_WAIT;
-                
+
           --metto done a 1 e aspetto start a 0.
             when S_DONE =>
-                o_en <= '0';  --chiudo scrittura dalla ram
+                o_en <= '0';  --chiudo scrittura/lettura dalla ram
                 o_we <= '0';
                 o_done <= '1';
                 if i_start = '0' then
                     current_state <= S_DONE2;
                 end if;
 
-             when S_DONE2 =>
-                o_done <= '0';
-               if i_start = '1' then
+             when S_DONE2 => --quando start è stato posto a 0
+                o_done <= '0'; --metto done a 1
+               if i_start = '1' then --se start torna a 1 in questo punto posso ri partire
                  current_state <= S_FETCH;
                 end if; 
             end case;
